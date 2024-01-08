fe887edf92379f8f3b931b661e7563dca12b3c7a
Dickson1g1 <bdickson1g1@gmail.com>
2024-01-08 12:10:44 -0500

    first commit

---
+++ mycode.py
@@ -0,0 +1,10 @@
+def main():
+    name = "Professor Nedd"
+    print("We want to know if you like programming!")
+    print()
+    print("Do you like programming "+ name + "?")
+    answer = input()
+    print("Grea! You said " + answer + "?")
+    print("Let's start learning some Python today")
+if __name__== "__main__":
+    main()
