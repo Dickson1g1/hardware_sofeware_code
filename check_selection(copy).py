a910289f7c9c8c19e8284ddf09c01b915905c33f
Dickson1g1 <bdickson1g1@gmail.com>
2024-01-24 13:54:12 -0500

    01/24/2024

--- check_selection(copy).py
+++ check_selection(copy).py
@@ -1,22 +1,21 @@
-def check_selection(numbers): # verify if it is a hexadecimal
+def check_selection(numbers):
     hex_list = ["A", "B", "C", "D", "E", "F", "0", "1",
                 "2", "3", "4", "5", "6", "7", "8", "9"]
     for number in numbers:
-        if number.upper()  in hex_list: #check if the number is not in hex list
-            print("This is a hexadecimal number")
-        elif:
-            print("That not a vlaid hexadecimal number")
-        else:()
-            return(True)
-
-    return(True)
+        if number.upper() in hex_list:  # modified the if statement
+            print("Good job! The input is a hexadecimal number!")
+            continue  # continue to the next iteration if it's not a hexadecimal number
+        else:
+            print("Not a hexadecimal number")
+            return True
+        return False
 
 def main():
-    good_selection = True
-    while  good_selection:
-        selection = input("Provide a hexadecimal number:")
+    good_selection = True  # modified the initial value
+    while good_selection:  # modified the loop condition
+        selection = input("Provide a hexadecimal number: ")
         good_selection = check_selection(selection)
-    print("Good job", selection, "is a hexadecimal number!!!")
+
 
 if __name__ == "__main__":
     main()
--- reverse_word2.py
+++ reverse_word2.py
@@ -1,8 +1,9 @@
 def intro_msg():
     print("I can reverse a string")
-    print("If you can give me 'apple' I will return 'elppa'")
-    print("I can even do an entire sentence")
-    return input("Type somthing and see:")
+    print("If you give me 'apple', I will return 'elppa'")
+    print("I can even reverse an entire sentence")
+    print("Or type done to quit")
+    return input("Type something and see: ")
 
 def reverse_word(characters):
     reverse_string = ''
@@ -11,9 +12,13 @@ def reverse_word(characters):
     return reverse_string
 
 def main():
-    word = intro_msg()
-    word = reverse_word(word)
-    print('Below is your string in reverse: \n{}'.format(word))
+    while True:
+        word = intro_msg()
+        if word.lower() in ['quit', 'done']:
+            break
+        reversed_word = reverse_word(word)
+        print('Below is your string in reverse:\n{}'.format(reversed_word))
+        print()  # Add a blank line for readability
 
 if __name__ == "__main__":
     main()
