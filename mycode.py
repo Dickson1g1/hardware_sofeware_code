c3846b78e6131adb32e3af131f4b21f6adabbf66
Dickson1g1 <bdickson1g1@gmail.com>
2024-01-30 11:13:44 -0500

    01/30/2024

--- mycode.py
+++ mycode.py
@@ -4,7 +4,7 @@ def main():
     print()
     print("Do you like programming "+ name + "?")
     answer = input()
-    print("Grea! You said " + answer + "?")
+    print("Great! You said " + answer + "?")
     print("Let's start learning some Python today")
 if __name__== "__main__":
     main()
