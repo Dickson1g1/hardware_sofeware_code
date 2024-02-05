5b9637e8f1f1524c8d9406aee14edea13f1bf38a
Dickson1g1 <bdickson1g1@gmail.com>
2024-02-05 10:18:33 -0500

    02/05/2024

--- dec_to_bin2.py
+++ dec_to_bin2.py
@@ -1,18 +1,20 @@
 def decimal_to_binary(number):
-    try:
-        result = ""
-        number = int(number)
-        while number > 0:
-            remainder = number % 2
-            number = number // 2
-            result = str(remainder) + result
-        return result
-    except ValueError:
-        print("Invalid input! Please enter a valid decimal number.")
+    result = ""
+    number = int(number)
+    while number > 0:   # keep dividing until at 0
+        remainder = number % 2
+        number = number // 2
+        result = str(remainder) + result   # place string in reverse order
+    return result
 
 def main():
-    num = input("Enter Decimal Number:")
-    print("Decimal {} to Binary: {}".format(num, decimal_to_binary(num)))
+    while True:
+        num = input("Enter Decimal Number: ")
+        if num.isdigit():
+            print("Decimal {} to Binary: {}".format(num, decimal_to_binary(num)))
+            break
+        else:
+            print("Invalid input! Please enter a valid decimal number.")
 
 if __name__ == "__main__":
     main()
